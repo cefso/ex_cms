74b4754eafa23c7ad4c22c131665b94a1d75c23f
cefso <w2799521098@outlook.com>
2023-08-01 20:55:33 +0800

    打印headers

--- code/web/alert.py
+++ code/web/alert.py
@@ -5,6 +5,8 @@ bp = Blueprint('alert', __name__, url_prefix='/alert')
 
 @bp.route('/post', methods=['POST'])
 def check():
+    headers = request.headers
     body = request.values.to_dict()
+    current_app.logger.info('请求的headers: {}'.format(headers))
     current_app.logger.info('接收到的内容: {}'.format(body))
     return {"message": "success"}
