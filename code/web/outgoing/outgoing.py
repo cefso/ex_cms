2857986c10987414b8efff0c54d65a9701062c76
cefso <w2799521098@outlook.com>
2023-08-09 17:07:38 +0800

    优化body数据类型

--- code/web/outgoing/outgoing.py
+++ code/web/outgoing/outgoing.py
@@ -10,9 +10,8 @@ bp = Blueprint('outgoing', __name__, url_prefix='/outgoing')
 @bp.route('outgoing', methods=['POST'])
 @siwa.doc(body=OutGoingSchema, tags=["outgoing"])
 def get_msg(body: OutGoingSchema):
-# def get_msg():
     headers = request.headers
-    body = request.get_data()
+    body = body.dict()
     current_app.logger.debug('请求的headers: {}'.format(headers))
     current_app.logger.debug('请求的body: {}'.format(body))
     header_sign = request.headers.get('sign')
