89fbb793fa8af77d62c8b5c6ab51b4c54cefa9f9
cefso <w2799521098@outlook.com>
2023-08-09 17:10:04 +0800

    优化日志输出

--- code/web/outgoing/outgoing.py
+++ code/web/outgoing/outgoing.py
@@ -14,6 +14,7 @@ def get_msg(body: OutGoingSchema):
     body = body.dict()
     current_app.logger.debug('请求的headers: {}'.format(headers))
     current_app.logger.debug('请求的body: {}'.format(body))
+    # 校验签名
     header_sign = request.headers.get('sign')
     check_sign = get_sign(request.headers.get('timestamp'))
     if header_sign != check_sign:
