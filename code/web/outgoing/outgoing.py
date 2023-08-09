29e0addb5d6174801f660587100524a73d3f8257
cefso <w2799521098@outlook.com>
2023-08-09 15:55:51 +0800

    取消body数据验证

--- code/web/outgoing/outgoing.py
+++ code/web/outgoing/outgoing.py
@@ -9,10 +9,11 @@ bp = Blueprint('outgoing', __name__, url_prefix='/outgoing')
 
 @bp.route('outgoing', methods=['POST'])
 @siwa.doc(header=SignHeaderSchema, body=OutGoingSchema, tags=["outgoing"])
-def get_msg(body: OutGoingSchema):
+# def get_msg(body: OutGoingSchema):
+def get_msg():
     headers = request.headers
     current_app.logger.debug('请求的headers: {}'.format(headers))
-    current_app.logger.debug('请求的body: {}'.format(body))
+    current_app.logger.debug('请求的body: {}'.format(request.values))
     header_sign = request.headers.get('sign')
     check_sign = get_sign(request.headers.get('timestamp'))
     if header_sign != check_sign:
