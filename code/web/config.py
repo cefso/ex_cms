17dc6c86ea4caf7aa71502c1765b21d8e825f4f7
cefso <w2799521098@outlook.com>
2023-08-15 23:11:31 +0800

    修改sql url获取方式

--- code/web/config.py
+++ code/web/config.py
@@ -5,14 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:  # 基本配置类
     SECRET_KEY = os.getenv('SECRET_KEY', 'some secret words')
-    ROBOT_APP_SECRET = os.getenv('ROBOT_APP_SECRET')
     FLASK_ENV = os.getenv('FLASK_ENV')
     ITEMS_PER_PAGE = 10
 
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = '***REMOVED***'
+    ROBOT_APP_SECRET = os.getenv('ROBOT_APP_SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SCHEDULER_API_ENABLED = True
@@ -20,7 +20,8 @@ class DevelopmentConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = '***REMOVED***'
+    ROBOT_APP_SECRET = os.getenv('ROBOT_APP_SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SCHEDULER_API_ENABLED = True
