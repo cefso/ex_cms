ad07401da9e416f12a36bc4d639058a2e4a97f69
cefso <w2799521098@outlook.com>
2023-10-26 17:03:53 +0800

    添加巡检相关内容

--- code/web/cms/cms.py
+++ code/web/cms/cms.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, current_app
 
 from web import siwa
 from .models import AliyunAuthSchema
-from .service import check_cms_all
+from .service import check_cms_all, cover_to_markdown
 
 bp = Blueprint('cms', __name__, url_prefix='/cms')
 
@@ -12,6 +12,7 @@ bp = Blueprint('cms', __name__, url_prefix='/cms')
 def check_cms(form: AliyunAuthSchema):
     headers = request.headers
     current_app.logger.debug('请求的headers: {}'.format(headers))
-    auth =form.dict()
+    auth = form.dict()
     check_info = check_cms_all(auth)
-    return check_info
+    markdown = cover_to_markdown(check_info)
+    return markdown
