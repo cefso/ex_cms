834e74ad12bdf3bd754327d24595e0451244a0e0
cefso <w2799521098@outlook.com>
2024-01-19 12:55:10 +0800

    修改地域默认值为上海

--- code/web/cms/models.py
+++ code/web/cms/models.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 class AliyunAuthSchema(BaseModel):
     access_key_id: str
     access_key_secret: str
-    endpoint: str = None
+    endpoint: str = 'metrics.cn-hangzhou.aliyuncs.com'
 
     class Config:
         orm_mode = True
